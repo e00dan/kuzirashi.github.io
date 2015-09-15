38b2827a88431f266a08dc4dd4ddeae9a5a1c779
Kuzirashi <kuzirashi@interia.pl>
2015-09-15 19:17:33 +0200

    Added lazy adding of pswp HTML to DOM. Fixed responsive menu.

--- app/controllers/apps/view.js
+++ app/controllers/apps/view.js
@@ -4,37 +4,6 @@ export default Ember.Controller.extend({
   actions: {
     showVideo() {
       this.set('showVideo', true);
-    },
-    showImages() {
-      const pswpElement = document.querySelectorAll('.pswp')[0],
-            items = [],
-            model = this.get('model');
-
-      if (!model) {
-        return;
-      }
-
-      const images = model.get('images');
-
-      images.forEach((image) => {
-        items.push({
-          src: image.get('id'),
-          w: image.get('width'),
-          h: image.get('height'),
-          title: image.get('caption')
-        });
-      });
-
-      // define options (if needed)
-      const options = {
-          // optionName: 'option value'
-          // for example:
-          index: 0 // start at first slide
-      };
-
-      // Initializes and opens PhotoSwipe
-      const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-      gallery.init();
     }
   }
 });
--- app/routes/apps/view.js
+++ app/routes/apps/view.js
@@ -1,19 +1,57 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    return new Promise((resolve) => {
-      this.store.findAll('app').then(() => {
-        resolve(this.store.findRecord('app', params.app_slug));
+  _showImages(providedElement) {
+    const pswpElement = providedElement || document.querySelectorAll('.pswp')[0],
+          items = [],
+          model = this.get('controller.model');
+
+    if (!model) {
+      return;
+    }
+
+    const images = model.get('images');
+
+    images.forEach((image) => {
+      items.push({
+        src: image.get('id'),
+        w: image.get('width'),
+        h: image.get('height'),
+        title: image.get('caption')
       });
+    });
+
+    // define options (if needed)
+    const options = {
+        // optionName: 'option value'
+        // for example:
+        index: 0, // start at first slide
+        history: false
+    };
+
+    // Initializes and opens PhotoSwipe
+    const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  },
+  model(params) {
+    return new Promise(resolve => {
+      this.store.findAll('app').then(() => resolve(this.store.findRecord('app', params.app_slug)));
     });
   },
-  /*afterModel() {
-    Ember.run.next(this, () => {
-      this.render('background-gallery', {
-        into: 'application',
-        outlet: 'body-child'
-      });
-    });
-  }*/
+  actions: {
+    showImages() {
+      const pswpElement = document.querySelectorAll('.pswp')[0];
+
+      if (!pswpElement) {
+        this.render('pswp', {
+          into: 'application',
+          outlet: 'body-direct'
+        });
+
+        return Ember.run.scheduleOnce('afterRender', this, '_showImages');
+      }
+
+      this._showImages(pswpElement);
+    }
+  }
 });
